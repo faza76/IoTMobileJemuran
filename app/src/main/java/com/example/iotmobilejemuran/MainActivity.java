7730c9046ff0b26c667649b73c46d2b0cb253f7b
jaka0839 <jaka.ahmad.tif17@polban.ac.id>
2019-07-12 07:17:26 +0700

    Merge branch 'master' of C:\Users\Jaka Ahmad Zakarya\AndroidStudioProjects\IoTMobileJemuran1 with conflicts.

--- app/src/main/java/com/example/iotmobilejemuran/MainActivity.java
+++ app/src/main/java/com/example/iotmobilejemuran/MainActivity.java
@@ -1,58 +1,23 @@
 package com.example.iotmobilejemuran;
 
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    String[] items;
-//    FloatingActionButton fab;
-    TypedArray img;
-
-    //Drawable[] img;
-
-
-    ArrayList<Jemuran> listJM = new ArrayList<Jemuran>();;
+    FloatingActionButton fab;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        Drawable draw = getResources().getDrawable(R.drawable.ic_directions_car_black_24dp);
-
-        Jemuran jm = new Jemuran("01","Device 01");
-        listJM.add(jm);
-
-//        String[] desc = {"Kendaraan versi darat","Kendaraan versi udara","kendaraan versi sehat","Kendaraan versi hemat"};;
-        String[] id = getResources().getStringArray(R.array.id);
-        //img = getResources().getDrawable(R.array.gambar,MainActivity.this);
-        TypedArray name = getResources().obtainTypedArray(R.array.name);
-
-
-
-        recyclerView = findViewById(R.id.Recyview);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-
-//        recyclerView.setAdapter(new Adapter(MainActivity.this,listJM));
-
-//        fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                NewDialog dialog = new NewDialog();
-//                dialog.show(getSupportFragmentManager(), "Hello");
-//            }
-//        });
-
-
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NewDialog dialog = new NewDialog();
+                dialog.show(getSupportFragmentManager(), "Hello");
+            }
+        });
     }
-
 }
